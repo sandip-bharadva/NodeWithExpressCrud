12518eb00143f04770e8f4d0058a07ab9bd61b47
sandip bharadva <63992929+sandip-bharadva@users.noreply.github.com>
2022-06-24 11:55:35 +0530

    Update User.js

--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,5 @@
+const { string } = require('joi');
 const { default: mongoose, now } = require('mongoose');
-
-var validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
 
 
 const userSchema = new mongoose.Schema({
@@ -17,14 +13,21 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         unique: true,
         required: 'Email address is required',
-        validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     password : {
-        type : String
+        type : String,
+        required : true
     },
     role : String,
     token: { type: String },
+    profile : {
+        type : String,
+        required : false
+    },
+    isVerified : {
+        type : Boolean,
+        default : false
+    },
     created_at : {
         type : Date,
         default : Date()
